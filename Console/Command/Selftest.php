add3555661a83b9d327819592eb0fb7327ed1004
Bruno Delsing <bruno_73@live.nl>
2022-05-18 11:50:08 +0200

    selftest w/ areacode

--- Console/Command/Selftest.php
+++ Console/Command/Selftest.php
@@ -1,17 +1,21 @@
 <?php
+
 /**
  * Copyright © Magmodules.eu. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 declare(strict_types=1);
 
 namespace Magmodules\Channable\Console\Command;
 
+use Magento\Framework\App\Area;
 use Magento\Framework\Console\Cli;
 use Magmodules\Channable\Api\Selftest\RepositoryInterface as SelftestRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Magento\Framework\App\State as AppState;
 
 /**
  * Class Selftest
@@ -30,14 +34,20 @@ class Selftest extends Command
      * @var SelftestRepository
      */
     private $selftestRepository;
+    /**
+     * @var AppState
+     */
+    private $appState;
 
     /**
      * Selftest constructor.
      *
      * @param SelftestRepository $selftestRepository
+     * @param AppState $appState
      */
     public function __construct(
-        SelftestRepository $selftestRepository
+        SelftestRepository $selftestRepository,
+        AppState $appState
     ) {
         $this->selftestRepository = $selftestRepository;
         parent::__construct();
@@ -58,6 +68,8 @@ class Selftest extends Command
      */
     public function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->appState->setAreaCode(Area::AREA_FRONTEND);
+
         $result = $this->selftestRepository->test();
         foreach ($result as $test) {
             if ($test['result_code'] == 'success') {
